66953879238480174a05d2907d2df73b518e0f73
mfelipe <mfelipe@gmail.com>
2025-07-21 17:31:58 +0100

    Schemas project with struct generation refactoring

--- schemas/generate-go-jsonschema.sh
+++ schemas/generate-go-jsonschema.sh
@@ -2,7 +2,7 @@
 set -e
 
 SCHEMA_DIR="$(dirname "$0")/schemas"
-CMD="go-jsonschema --struct-name-from-title --extra-imports --only-models"
+CMD="go-jsonschema --struct-name-from-title  --extra-imports --only-models --resolve-extension .json"
 FILES=""
 
 for file in "$SCHEMA_DIR"/*.json; do
@@ -13,11 +13,10 @@ for file in "$SCHEMA_DIR"/*.json; do
   NAME=$(echo "$SCHEMA_ID" | awk -F/ '{print $(NF-1)}')
   RAW_VERSION=$(echo "$SCHEMA_ID" | awk -F/ '{print $NF}')
   VERSION=v$(echo "$RAW_VERSION" | sed 's/\./_/g')
-  GO_FILE="models/${NAME}/${VERSION}/${SCHEMA_FILE%.json}.go"
-  mkdir -p "models/${NAME}/${VERSION}"
+
   CMD+=" \
-  --schema-package=${SCHEMA_ID}=github.com/mfelipe/go-feijoada/schemas/models/${NAME}/${VERSION} \
-  --schema-output=${SCHEMA_ID}=models/${NAME}/${VERSION}/${SCHEMA_FILE%.json}.go"
+  --schema-package=${SCHEMA_ID}=github.com/mfelipe/go-feijoada/schemas/models/${VERSION} \
+  --schema-output=${SCHEMA_ID}=models/${VERSION}/${NAME}.go"
   FILES+=" schemas/${SCHEMA_FILE}"
 done
 
